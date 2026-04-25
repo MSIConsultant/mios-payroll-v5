8045fffdd50ddd3e3476288a066e7afcde1e3ca3
MSIConsultant <msiconsultant.international@gmail.com>
2026-04-25 21:39:41 +0700

    Add metadata and change HTML language attribute

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,19 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import type { Metadata } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'MIOS Payroll',
+  description: 'MIOS Payroll Management System',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en">
+    <html lang="id">
       <body>{children}</body>
     </html>
-  )
+  );
 }
